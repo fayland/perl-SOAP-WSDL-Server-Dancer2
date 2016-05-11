a401ecd0ba92d35e7ba22f775c5b54e2da3ea93c
Fayland Lam <fayland@gmail.com>
2016-05-11 13:52:42 +0800

    first commit

---
+++ lib/SOAP/WSDL/Server/Dancer2.pm
@@ -0,0 +1,39 @@
+package SOAP::WSDL::Server::Dancer2;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SOAP::WSDL::Server::Dancer2 - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use SOAP::WSDL::Server::Dancer2;
+
+=head1 DESCRIPTION
+
+SOAP::WSDL::Server::Dancer2 is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
